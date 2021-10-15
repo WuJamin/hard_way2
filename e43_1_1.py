15d43d46ef2fc7ad0355bd773e4065962e5fcbef
andy <wy_andy@126.com.cn>
2021-10-15 11:05:55 +0800

    v43.2

--- e43_1_1.py
+++ e43_1_1.py
@@ -30,6 +30,9 @@ class Engine():
 
     
     def play(self):
+        #没有在方法里引入他类作为参数
+        #而是在类中引入属性，属性实参为他类的实例
+        #好处，类内可能有很多方法，类属性中引入，则类内其他方法也能用
         current_room = 'rooma'
         while current_room != 'roomc':
             current_room = self.choose.chooseing()
@@ -40,4 +43,8 @@ class Engine():
 
 choose = Chooser()
 game = Engine(choose)
+#y=f(x),x要可变
+#则x是另一个类Choose的实例，
+#x可使用Choose类的方法来改变返回的值
+# ，x=g(z)
 game.play()
